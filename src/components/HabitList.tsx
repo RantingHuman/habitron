baa09dc89c7b5c61be5f69606583ccb557815f19
Shrivas Vasudevan <shrivas@gmail.com>
2024-06-18 12:44:34 -0700

    Testing Manifest backend - WIP

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -3,15 +3,29 @@ import HabitListItem from './HabitListItem';
 import HabitListHeader from './HabitListHeader';
 import Button from './buttons/Button';
 import { NavLink } from 'react-router-dom';
+import Manifest from '@mnfst/sdk';
+import { useEffect, useState } from 'react';
+import { Habit } from '../types';
 
 const HabitList = () => {
   const { habits, resetHabits } = useHabitronStore();
+  const [manifestData, setManifestData] = useState<Habit[]>([]);
+
+  useEffect(() => {
+    const manifest = new Manifest();
+
+    manifest.from("habits")
+    .find<Habit>()
+    .then((res) => {
+      setManifestData(res);
+    })
+  },[]);
   return (
   habits.length > 0 ?
     <div>
       
       {<HabitListHeader />}
-      {habits.map((habit) => (
+      {manifestData.map((habit) => (
         <HabitListItem key={habit.id} habit={habit} />
       ))}
       <Button appearance="danger" name="reset-habits" onClick={resetHabits}>Reset Habits</Button>
